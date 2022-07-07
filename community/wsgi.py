1deec94a875f11b5b816b0ee255c68031cbfd426
wazeer1 <88919483+wazeer1@users.noreply.github.com>
2022-07-06 21:45:25 -0700

    Update wsgi.py

--- community/wsgi.py
+++ community/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'community.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root="/path/to/static/files")
+application.add_files("/path/to/more/static/files", prefix="more-files/")
