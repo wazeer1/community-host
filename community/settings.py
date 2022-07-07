7d3b35d02c29de443528feaeb480d9c24d50e11b
wazeer1 <88919483+wazeer1@users.noreply.github.com>
2022-07-07 06:18:56 -0700

    Update settings.py

--- community/settings.py
+++ community/settings.py
@@ -164,6 +164,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL='/media/'
+
+MEDIA_ROOT=[
+    'mymedia'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
